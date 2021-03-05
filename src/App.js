560865acc73004739614c0b25bab2a9acb5dbea6
kunaldhingra2209 <kunaldhingra2209@users.noreply.github.com>
2021-03-05 12:12:48 +0530

    kd

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-         <h1>React</h1>
+         <h1>React Kunal</h1>
          <Router>
            <Link to="">Home   </Link>
            <Link to="/profile">  Profile</Link>
